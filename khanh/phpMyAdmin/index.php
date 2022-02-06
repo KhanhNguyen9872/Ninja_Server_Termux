b1bafd7b4d774c3fc48a3fee65b3d4f3244b6b1f
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-02-06 11:14:22 +0700

    Add files via upload

---
+++ khanh/phpMyAdmin/index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Không có phpMyAdmin đâu mà mò:vvv\r\n";
+echo "<br>";
+echo "<br>";
+echo "NinjaSchoolOnTermux\r\n";
+echo "<br>";
+echo nl2br("\n Nguyễn Văn Khánh (KhanhNguyen9872) \n Github: https://github.com/khanhnguyen9872 \r\n Facebook: https://fb.me/khanh10a1");
+
+?>
