7da80e6e7512b9a74c9c0dfe4908d4f61af8d358
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-02-06 11:45:37 +0700

    Add files via upload

---
+++ CONF_FILE/sql.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+option="${1}" 
+case ${option} in 
+   --start) 
+	   if ps -C httpd >/dev/null; then
+		   echo "MySQL is still running...."
+	   else
+			httpd &> /dev/null
+			mysqld --skip-grant-tables --general-log &> /dev/null
+	   fi
+      ;; 
+   --stop)
+	   killall httpd
+	   killall mysqld &> /dev/null
+	   killall /data/data/com.termux/files/usr/bin/mariadbd &> /dev/null
+	   killall mysqld_safe &> /dev/null
+      ;;
+   *)  
+      printf "\nCOMMAND: sql [ARGURMENT]\n\n"
+      printf "  --start [Start MySQL]\n"
+      printf "  --stop  [Stop MySQL]\n"
+      exit 1
+      ;; 
+esac
+#ib_logfile0
