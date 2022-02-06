047142bfbc7d6578496a7b140009609e5c7b3488
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-02-06 14:10:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,6 +89,33 @@ khanh() {
 	chmod 777 ../usr/share/apache2/default-site/htdocs 2> /dev/null
 }
 
+install_server() {
+	echo " - Dang tai ve...."
+	clear
+	cd 2> /dev/null
+	wget -O image-khanhnguyen9872.img "https://github.com/KhanhNguyen9872/Ninja_Server_Termux/releases/download/NinjaServerTermuxv01/image-khanhnguyen9872.img"
+	wget -O image-khanhnguyen9872.sha512sum "https://github.com/KhanhNguyen9872/Ninja_Server_Termux/releases/download/NinjaServerTermuxv01/image-khanhnguyen9872.sha512sum"
+	clear
+	echo " - Dang kiem tra.... "
+	sha512sum -c image-khanhnguyen9872.sha512sum || {
+            printf "${red} File cai dat bi hong. Vui long cai lai \n${reset}"
+	    rm -f image-khanhnguyen9872.img 2> /dev/null
+	    rm -f image-khanhnguyen9872.sha512sum 2> /dev/null
+            exit 1
+        }
+	echo " - Dang cai dat...."
+	mkdir ../usr/share/KhanhNguyen9872 2> /dev/null
+	mv image-khanhnguyen9872.img ../usr/share/KhanhNguyen9872/image-khanhnguyen9872.tar.xz 2> /dev/null
+	cd ../usr/share/KhanhNguyen9872 2> /dev/null
+	proot --link2symlink tar -xJf image-khanhnguyen9872.tar.xz 2> /dev/null || :
+	rm -f image-khanhnguyen9872.tar.xz 2> /dev/null
+	cd 2> /dev/null
+	mv Ninja_Server_Termux/CONF_FILE/ninja.sh ../usr/bin/ninja 2> /dev/null
+	chmod 777 ../usr/bin/ninja 2> /dev/null
+	rm -rf Ninja_Server_Termux 2> /dev/null
+	exit 0
+}
+
 clear
 install_Packages
 configure_Files
@@ -96,3 +123,4 @@ document_Root
 set_Up_files
 install
 khanh
+install_server
