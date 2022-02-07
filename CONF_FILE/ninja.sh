7b7c5f99c72ca471ff79e7add877d2db03644a0a
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-02-07 13:35:39 +0700

    Update ninja.sh

--- CONF_FILE/ninja.sh
+++ CONF_FILE/ninja.sh
@@ -1,11 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #KhanhNguyen9872
-cd 2> /dev/null
+cd ~/../usr/share/KhanhNguyen9872 2> /dev/null
 unset LD_PRELOAD
 command="proot"
 command+=" --link2symlink"
 command+=" -0"
-command+=" --kill-on-exit"
 command+=" -r ubuntu-fs"
 if [ -n "$(ls -A ubuntu-binds)" ]; then
     for f in ubuntu-binds/* ;do
@@ -21,5 +20,4 @@ command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/us
 command+=" TERM=$TERM"
 command+=" LANG=C.UTF-8"
 command+=" /bin/bash --login"
-com="$@"
 $command -c "khanh"
