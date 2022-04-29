d4bf147304df2474252fe3102a59362c4b992bb7
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-04-29 20:53:20 +0700

    Create khanhupdate.sh

---
+++ khanhupdate.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+printf "\n\n Nói chung là phiên bản mới này sử dụng môi trường tamp mới nên bạn không thể update từ version trước 6.5, bạn nên vào menu, backup lại data chơi của bạn, sau đó hãy cài đặt lại tamp để có trải nghiệm tốt nhất, ít lỗi nhất nhé!\n\n Đừng quên sau khi cài lại phải restore data trong menu đấy, nếu k là chơi lại đó hehe!\n\n From KhanhNguyen9872 with love <3\n\n"
+exit 0
