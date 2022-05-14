d76392de3218c2ead2526e327147b4ca6ab24b1d
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-05-14 20:42:08 +0700

    Delete index.php

--- khanh/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "NinjaSchoolOnTermux\r\n";
-echo "<br>";
-echo nl2br("\n Nguyễn Văn Khánh (KhanhNguyen9872) \n Github: https://github.com/khanhnguyen9872 \r\n Facebook: https://fb.me/khanh10a1");
-
-?>
