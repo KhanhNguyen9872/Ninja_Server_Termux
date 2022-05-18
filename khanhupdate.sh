21193bb88841b2ed5fcfb1e0d5091db1ad2aec9e
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-05-18 22:14:49 +0800

    Update khanhupdate.sh

--- khanhupdate.sh
+++ khanhupdate.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
-printf "\nCảm ơn bạn đã tin tưởng và sử dụng Ninja-Server-Termux!\n Hiện tại đang là phiên bản 7.0, và phiên bản này sẽ có độ tương thích tốt hơn cho nhiều dòng máy từ 32bit đến 64bit!\n Vì thế nếu bạn muốn update lên 7.0, hãy xóa Termux và cài đặt lại!\n Trân trọng cảm ơn!\n\n"
+printf "\nCảm ơn bạn đã tin tưởng và sử dụng Ninja-Server-Termux!\n Hiện tại đang là phiên bản 7.0, và phiên bản này sẽ có độ tương thích tốt hơn cho nhiều dòng máy từ 32bit đến 64bit!\n Vì thế nếu bạn muốn update lên 7.0, hãy xóa Termux và cài đặt lại!\nĐừng quên backup lại data chơi của bạn trong menu nhé! Nếu không bạn sẽ phải chơi lại đấy!\n Trân trọng cảm ơn!\n\n"
 exit 0
